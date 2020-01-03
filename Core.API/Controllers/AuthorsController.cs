246b0156767f6234ce2477981904c339987b721c
jameson.trinker <jameson.trinker@cellularsales.com>
2020-01-03 11:19:08 -0500

    code for pluralsight Building a RESTful API with ASP.NET Core 3

---
+++ Core.API/Controllers/AuthorsController.cs
@@ -0,0 +1,63 @@
+﻿using Core.API.Helpers;
+using Core.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        /// <summary>
+        /// creating private read only instance of our DbContext since we won't be changing it
+        /// </summary>
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        /// <summary>
+        /// constructor method which injects our instance of our Repository into our controller
+        /// </summary>
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors() // ActionResult is returning type IEnumberable<AuthorDto>
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            var authors = new List<AuthorDto>();
+
+            foreach (var author in authorsFromRepo)
+            {
+                authors.Add(new AuthorDto()
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    MainCategory = author.MainCategory,
+                    Age = author.DateOfBirth.GetCurrentAge()
+                });
+            }
+            //return new JsonResult(authorsFromRepo);
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(authorFromRepo);
+        }
+    }
+}
